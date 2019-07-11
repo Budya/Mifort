d73e3414d1e73cb795680ee47abc7ca93d0a03a7
Budya <budya2012@gmail.com>
2019-07-11 09:22:11 +0300

    removed some unnecessary lines

---
+++ 03_ValidatorApp/03_ValidatorApp/app.ts
@@ -0,0 +1,82 @@
+﻿class Person {
+
+    name: string;
+
+    constructor(name: string) {
+        this.name = name;
+    }
+
+}
+
+let validationValue: string;
+let validState: string[] = null
+
+let csv: ColumnDescriptor[] = [
+    {
+        fieldName: "name",
+        validators: [
+            length(2, 7),
+        ]
+
+    },
+]
+
+class Validator {
+
+    colDescriptor: ColumnDescriptor[];
+
+    constructor(csv: ColumnDescriptor[]) {
+        this.colDescriptor = csv;
+    }
+
+    validate(obj: Person): string[] {
+        validState = null;
+        let objectKeys: string[] = Object.keys(obj);
+        for (let i = 0; i < objectKeys.length; i++) {
+            for (let j = 0; j < csv.length; j++) {
+                if (objectKeys[i] === csv[j].fieldName) {                    
+                    validationValue = obj[objectKeys[i]];
+                    for (let currentValidationRule = 0; currentValidationRule < csv[j].validators.length; currentValidationRule++) {
+                        csv[j].validators[currentValidationRule];
+                    }
+                }
+
+            }
+
+        }
+
+        return validState;
+    }
+}
+
+let person: Person = new Person("Steve");
+
+let validator = new Validator(csv);
+
+validator.validate(person);
+
+interface Validators {
+
+    validators: any[];
+
+}
+
+interface ColumnDescriptor extends Validators {
+    fieldName: string;
+    validators: any[];
+}
+
+
+
+function length(min: number, max: number): void {
+
+    console.log(validationValue);
+    if (validationValue.length >= min && validationValue.length <= max) {
+        validState.push("name lenght is good");
+    }
+}
+
+
+
+
+
